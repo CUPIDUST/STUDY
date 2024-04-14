219773c5960eb40b1cbad2ddc9744a0ff71e2c1d
CUPIDUST <sandipbehera681@gmail.com>
2024-04-14 21:27:57 +0530

    UPDATE IN cudtomCursor and instaStory

--- webProject/customCursor/01.js
+++ webProject/customCursor/01.js
@@ -1,7 +1,6 @@
 let container1 = document.querySelector(".container1");
 let cursor = document.querySelector(".cursor");
 container1.addEventListener("mousemove", (dex) => {
-    // console.log(dex.x );
     cursor.style.left = dex.x + "px";
     cursor.style.top  = dex.y + "px";
 });
--- webProject/instaStory/01.js
+++ webProject/instaStory/01.js
@@ -1,32 +1,29 @@
-let image = [0, 1, 2, 3]
-// console.log(image);
-let myobj = [{ dp: "Anna3.jpg",story:"Anna20.jpg" }, 
-{ dp: "Anna3.jpg",story:"Anna20.jpg" },
- { dp: "Anna3.jpg",story:"Anna20.jpg" },
-  { dp: "Anna3.jpg",story:"Anna20.jpg" }]
+let arr1 = [1, 0, 2, 3];
+let myobj = [{ dp: "Anna3.jpg", story: "Anna20.jpg" },
+{ dp: "Anna3.jpg", story: "Anna20.jpg" },
+{ dp: "Anna3.jpg", story: "Anna20.jpg" },
+{ dp: "Anna3.jpg", story: "Anna20.jpg" }];
 let clutter = "";
-image.forEach((element, idx) => {
+arr1.forEach((element, idx) => {
     // console.log(element);
-    // element.addEventListener("click", () => {
-    //     // console.log("HI");
-    //     element.src = "Anna20.jpg";
-
-    //     element.src="myobj.story1";
-    // })
-    clutter += `<div class="photo1 storyBox"><img src="${myobj[idx].dp}" alt="yo"></div>`
-    // <div class="photo2 storyBox"><img src="${element.story}" alt="yo"></div>
-    // <div class="photo3 storyBox"><img src="${element.story}" alt="yo"></div>
-    // <div class="photo4 storyBox"><img src="${element.story}" alt="yo"></div>
+    // console.log(idx);
+    clutter += `<div id="${idx}" class="storyBox"><img src="${myobj[idx].dp}" alt="yo"></div>`
 });
-// console.log(image[1]);
-// let a=7;
-// console.log(`myobj.story${a}`);
-// console.log(clutter);
 document.querySelector(".container1").innerHTML = clutter;
-// console.log(myobj[0]);
-let store=document.querySelector(".storyBox img")
-store.forEach((element,idx)=>{
-    element.addEventListener("click",()=>{
-        element.src="Anna20.jpg";
+let storyImg = document.querySelector(".storyBox img");
+storyImg = Array.from(storyImg);
+storyImg.forEach((element, idx) => {
+    element.addEventListener("click", () => {
+        // element.src = "Anna20.jpg";
+        element.src = "myobj[idx].story";
+    })
+})
+let story = document.querySelectorAll(".storyBox");
+story=Array.from(story);
+// console.log(story);
+let arr2 = [0, 1, 2, 3];
+arr2.forEach((element) => {
+    story[element].addEventListener("click", () => {
+        console.log("HELLO");
     })
 })
