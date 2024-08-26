1dc110781be6e21bdc259c3ce24a59ff03e3685e
CUPIDUST <sandipbehera681@gmail.com>
2024-08-26 14:53:49 +0530

    Updated STUDY DIRECTORY

--- impJava/p1.java
+++
@@ -1,7 +0,0 @@
-// package WOW;
-public class p1{
-    public static void main(String[] args) {
-        int a=10;
-        System.out.println(a);
-    }
-}
--- impJava/p2.java
+++
@@ -1,6 +0,0 @@
-// package WOW;
-public class p2 {
-    public static void main(String[] args) {
-        System.out.println("HI");
-    }
-}
